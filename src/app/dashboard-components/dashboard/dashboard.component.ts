522e92b1975d63e59425d1954f2bc865beeb2e62
waleedo0o7 <waleedhd1@gmail.com>
2021-06-07 20:01:03 +0200

    test finger print

--- src/app/dashboard-components/dashboard/dashboard.component.ts
+++ src/app/dashboard-components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor( private http:HttpClient  , private router : Router) {}
 
+
+  username = localStorage.getItem('username');
+  password =  localStorage.getItem('password');
+  checkInOutData:any;
+
+  checkInOut(){
+    let url = "http://5.9.120.68:81/api/Attendance/Check";
+    let headers = { 'content-type': 'application/json'};
+    let body= {"userName":  this.username ,"password": this.password };
+
+    return this.http.post( url , body , {headers : headers}).subscribe( data => { 
+      this.checkInOutData = data;
+      console.log(this.checkInOutData);
+      
+    } , error => {
+      console.log(error.error);
+      
+    })
+  }
+  
   ngOnInit(): void {
   }
 
 }
+
+
+
+
+
+
+
+
+
